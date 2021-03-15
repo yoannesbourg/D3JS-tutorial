bd60150c0db8e936dcf1d1bda336a7e1f90df7b7
Yoann <yoannpascalbouix@gmail.com>
2021-03-15 18:25:17 +0100

    Update data onclick

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import { select } from "d3"
 
 function App() {
+  const [data, setData] = useState([25, 30, 45, 60, 20])
+  const svgRef = useRef()
+
+  useEffect(() => {
+    console.log(svgRef)
+    const svg = select(svgRef.current)
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", value => value)
+      .attr("cx", value => value * 2)
+      .attr("cy", value => value * 2)
+      .attr("stroke", "red")
+  }, [data])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg ref={svgRef}>
+      </svg>
+      <br />
+      <button onClick={() => setData(data.map(value => value + 5))}>Update data</button>
+      <button onClick={() => setData(data.filter(value => value <= 35))}>Filter data</button>
     </div>
   );
 }
